076935936ec26889d5e4bce19d74fe2c9f778558
igorthedci <foumart@ukr.net>
2018-10-30 17:25:47 +0200

    python_02 test_role_create

--- test_base.py
+++ test_base.py
@@ -8,16 +8,18 @@ class BaseTests(unittest.TestCase):
     def setUpClass(cls):
         cls.base_url = "http://pulse-rest-testing.herokuapp.com/"
 
-    def test_base_url(self):
+    def test_base_url(self):  # is the link is live
         response = requests.get(self.base_url)
         self.assertEqual(response.status_code, 200)
         return
 
-    def test_base_links(self):
+    def test_base_links(self):  # the link contains two elements
         response = requests.get(self.base_url)
         response_body = response.json()
         expected_body = {"Roles": self.base_url + "roles", "Books": self.base_url + "books"}
         self.assertEqual(response_body, expected_body)
-        return 
+        return
 
 
+if __name__ == "__main__":
+    unittest.main(verbosity=2)
--- test_roles.py
+++ test_roles.py
@@ -35,125 +35,24 @@ class RolesTests(unittest.TestCase):
             requests.delete(self.url + "/" + str(role_id))
         return
 
-    def dict_contains(a={}, b={}):
-        """
-        :param a: dictionary
-        :param b: dictionary
-        :return: TRUE if A-dict contains B-dict
-        """
-        result = True
-        for key in b:
-            if key not in a or a[key] != b[key]:
-                result = False
-                break
-        return result
-
-    # url_books = 'http://pulse-rest-testing.herokuapp.com/books'
-    # url_roles = 'http://pulse-rest-testing.herokuapp.com/roles'
-
 # 1● Создаёт персонажа POST /roles/, вы запоминаете его id.
 # 2● Проверяете, что он создался и доступен по ссылке GET /roles/[id]
-# 3● Проверяете, что он есть в списке персонажа по GET /roles/
+# 3● Проверяете, что он есть в списке персонажей по GET /roles/
     def test_role_create(self):
         for role in self.roles:
-            with self.subTest(role=self.roles):
-                response = requests.post(self.url, data=role)
-                self.assertEqual(response.status_code, 201)
+            with self.subTest(role=role):
+                response = requests.post(self.url, data=role)  # create an item
+                self.assertEqual(response.status_code, 201)  # check code === 201
                 body = response.json()
-                # self.assertDictContainsSubset(role, response.json(), )
-                for key in role:
-                    self.assertEqual(role[key].strip(), body[key])
-                # TODO GET запросы
-                self.role_ids.append(body["id"])
+                self.role_ids.append(body["id"])  # save id for tearDown()
                 #
-                r = requests.get(self.base_url + 'role/' + str(body["id"]))
-        hero_01 = {'name': 'Name1', 'type': 'type1', 'level': 1, 'role': 2}
-        hero_01_id = requests.post(url_roles, hero_01).json()['id']
-        print('Создан персонаж с номером', hero_01_id)
-#    hero_01_id = 12
-
-# 2● Проверяете, что он создался и доступен по ссылке GET /roles/[id]
-    hero_test_url = url_roles + '/' + str(hero_01_id)
-    hero_test = requests.get(hero_test_url).json()
-    marker = dict_contains(hero_test, hero_01)
-    print('Персонаж', 'существует' if marker else 'отсутствует.')
-
-# 3● Проверяете, что он есть в списке персонажа по GET /roles/
-    hero_list = requests.get(url_roles).json()
-    marker = False
-    for hero_test in hero_list:
-        marker = dict_contains(hero_test, hero_01)
-        if marker: break
-    print('Персонаж', 'существует' if marker else 'отсутствует.')
-
-# 4● Изменяете этого персонажа методом PUT roles/[id]/
-    hero_01 = {'name': 'Name1new', 'type': 'type1new', 'level': 2, 'book': 2}
-    hero_test_url = url_roles + '/' + str(hero_01_id)
-    hero_test = requests.put(hero_test_url, hero_01).json()
-    marker = dict_contains(hero_test, hero_01)
-    print('Персонаж', 'обновлен' if marker else 'остался прежним.')
-
-# 5● Проверяете, что он изменился и доступен по ссылке /roles/[id]
-    hero_test_url = url_roles + '/' + str(hero_01_id)
-    hero_test = requests.get(hero_test_url).json()
-    marker = dict_contains(hero_test, hero_01)
-    print('Изменение', 'правильно.' if marker else 'отсутствует.')
-
-# 6● Проверяете, что он есть в списке персонажа по GET /roles/ с новой инфой
-    hero_list = requests.get(url_roles).json()
-    marker = False
-    for hero_test in hero_list:
-        marker = dict_contains(hero_test, hero_01)
-        if marker: break
-    print('Изменение', 'правильно.' if marker else 'отсутствует.')
-
-# 7● Удаляете этого персонажа методом DELETE roles/[id]
-    hero_test_url = url_roles + '/' + str(hero_01_id)
-    hero_test = requests.delete(hero_test_url)
-    print('Персонаж', hero_01_id, 'удален.' if hero_test.status_code == 204 else 'выжил.')
-
-# 8● Второй скрипт: тоже самое с книгами
-    book_01 = {'title': 'New Book', 'author': 'New Author'}
-    book_01_id = requests.post(url_books, book_01).json()['id']
-#    book_01_id = 820
-    print('Создана книга с номером', book_01_id)
-#
-    book_test_url = url_books + '/' + str(book_01_id)
-    book_test = requests.get(book_test_url).json()
-    marker = dict_contains(book_test, book_01)
-    if not marker:
-        print('Запрос:', book_01)
-        print('Ответ:', book_test)
-    print('Книга', 'существует' if marker else 'отсутствует.')
-#
-    book_list = requests.get(url_books).json()
-    marker = False
-    for book_test in book_list:
-        marker = dict_contains(book_test, book_01)
-        if marker: break
-    print('Книга', 'существует' if marker else 'отсутствует.')
-#
-    book_01 = {'title': 'Next Book', 'author': 'Edition 2'}
-    book_test_url = url_books + '/' + str(book_01_id)
-    book_test = requests.put(book_test_url, book_01).json()
-    marker = dict_contains(book_test, book_01)
-    print('Книга', 'переписана' if marker else 'осталась прежней.')
-#
-    book_test_url = url_books + '/' + str(book_01_id)
-    book_test = requests.get(book_test_url).json()
-    marker = dict_contains(book_test, book_01)
-    print('Изменение', 'правильно.' if marker else 'отсутствует.')
-#
-    book_list = requests.get(url_books).json()
-    marker = False
-    for book_test in book_list:
-        marker = dict_contains(book_test, book_01)
-        if marker: break
-    print('Изменение', 'правильно.' if marker else 'отсутствует.')
-#
-    book_test_url = url_books+'/'+str(book_01_id)
-    book_test = requests.delete(book_test_url)
-    print('Книга', book_01_id, 'удалена.' if book_test.status_code == 204 else 'сохранилась.')
+                for key in role:
+                    self.assertEqual(str(role[key]).strip(), str(body[key]))
+                #
+                body = requests.get(self.url + '/' + str(body["id"])).json()
+                for key in role:
+                    self.assertEqual(str(role[key]).strip(), str(body[key]))
+        return True
 
 
 if __name__ == "__main__":

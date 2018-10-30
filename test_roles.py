4aabd8bcd7a9bf70d3cba4616fa3cbd66d44c9f5
igorthedci <foumart@ukr.net>
2018-10-30 19:46:44 +0200

    hw07 ROLES: positive & negative tests @elpiankova, please review

--- test_roles.py
+++ test_roles.py
@@ -17,7 +17,7 @@ HW06_01
 """
 
 
-class RoleTests(unittest.TestCase):
+class RolePositiveTests(unittest.TestCase):
 
     @classmethod
     def setUpClass(cls):
@@ -54,6 +54,7 @@ class RoleTests(unittest.TestCase):
         for role in self.roles:
             with self.subTest(role=role):
                 response = requests.post(self.url, data=role)  # create an item
+                self.assertEqual(response.status_code, 201)  # check code === 201
                 body = response.json()
                 self.role_ids.append(body["id"])  # save id for tearDown()
                 #
@@ -67,6 +68,7 @@ class RoleTests(unittest.TestCase):
         for role in self.roles:
             with self.subTest(role=role):
                 response = requests.post(self.url, data=role)  # create an item
+                self.assertEqual(response.status_code, 201)  # check code === 201
                 body = response.json()
                 self.role_ids.append(body["id"])  # save id for tearDown()
                 #
@@ -74,7 +76,9 @@ class RoleTests(unittest.TestCase):
                     if str(body[key]).isalpha():
                         body[key] += 'update'
                 #
-                body = requests.put(self.url + '/' + str(body["id"])).json()
+                response = requests.put(self.url + '/' + str(body["id"]))
+                body = response.json()
+                self.assertEqual(response.status_code, 201)  # check code === 201
                 for key in role:
                     self.assertEqual(str(role[key]).strip(), str(body[key]))
         return True
@@ -84,13 +88,102 @@ class RoleTests(unittest.TestCase):
         for role in self.roles:
             with self.subTest(role=role):
                 response = requests.post(self.url, data=role)  # create an item
+                self.assertEqual(response.status_code, 201)  # check code === 201
                 body = response.json()
                 self.role_ids.append(body["id"])  # save id for tearDown()
                 #
                 response = requests.delete(self.url + '/' + str(body["id"]))
                 self.assertEqual(response.status_code, 204)  # check code === 204
+        return True
+
+
+class RoleNegativeTests(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        cls.base_url = "http://pulse-rest-testing.herokuapp.com"
+        cls.url = cls.base_url + "/roles"
+        cls.keys = ['name', 'type', 'level', 'book']
+
+    def setUp(self):
+        self.invalid_roles = [
+            {'name': '', 'type': 'type1', 'level': 1, 'book': 2},
+            {'name': 'Name1', 'type': '', 'level': 1, 'book': 2},
+            # {'name': 'Name1', 'type': 'type1', 'level': -1, 'book': 2},
+            {'name': 'Name1', 'type': 'type1', 'level': 1, 'book': 0},
+            # {'name': 'Name1', 'type': 'type1', 'book': 2},
+            {'name': 'Name1', 'type': 'type1', 'level': 'aasd', 'book': 2}
+        ]
+        self.valid_roles = [
+            {'name': 'Name1', 'type': 'type1', 'level': 1, 'book': 2}
+        ]
+        self.role_ids = []
+        return
+
+    def tearDown(self):
+        for role_id in self.role_ids:
+            requests.delete(self.url + "/" + str(role_id))
+        return
+
+    def isRoleValid(self, role={}):
+        if len(role) != 4:
+            return False
+        akeys = role.keys()
+        for akey in akeys:
+            if akey not in self.keys:
+                return False
+        # check NAME, TYPE
+        for i in range(2):
+            ttt = role[self.keys[i]]
+            if type(ttt) != str:
+                return False
+            ttt = ttt.strip()
+            if not ttt.isalpha():
+                return False
+        # check LEVEL, BOOK
+        for i in [2, 3]:
+            ttt = role[self.keys[i]]
+            if type(ttt) != int:
+                return False
+            if ttt < 1:
+                return False
+        #
+        return True
+
+# 1● Создаёт персонажа POST /roles/, вы запоминаете его id.
+    def test_role_create(self):
+        for role in self.invalid_roles:
+            with self.subTest(role=role):
+                # self.assertFalse(isRoleValid(role))
+                response = requests.post(self.url, data=role)  # create an item
+                if response.status_code == 201:
+                    body = response.json()
+                    self.role_ids.append(body["id"])  # save id for tearDown()
+                    print(body)
+                self.assertNotEqual(response.status_code, 201)  # check code === 201
                 #
-                response = requests.delete(self.url + '/' + str(body["id"]))
+        return True
+
+# 2● Проверяете, что отсутствующий персонаж недоступен по ссылкам GET/PUT/DELETE /roles/[id]
+    def test_role_read(self):
+        for role in self.valid_roles:
+            with self.subTest(role=role):
+                response = requests.post(self.url, data=role)  # create an item
+                self.assertEqual(response.status_code, 201)  # check code === 201
+                body = response.json()
+                self.role_ids.append(body["id"])  # save id for tearDown()
+                url_role = self.url + '/' + str(body["id"])
+                #
+                response = requests.delete(url_role)
+                self.assertEqual(response.status_code, 204)  # check code === 204
+                #
+                response = requests.get(url_role)
+                self.assertEqual(response.status_code, 404)  # check code === 404
+                #
+                response = requests.put(url_role)
+                self.assertEqual(response.status_code, 404)  # check code === 404
+                #
+                response = requests.delete(url_role)
                 self.assertEqual(response.status_code, 404)  # check code === 404
         return True
 

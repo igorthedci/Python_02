d7db7bef6856a145972795dfe9c2c874f0208e30
igorthedci <foumart@ukr.net>
2018-11-09 12:15:26 +0200

    refactoring

--- test_books.py
+++ test_books.py
@@ -2,7 +2,7 @@ import requests
 import unittest
 
 
-class BooksTests(unittest.TestCase):
+class BooksPositiveTests(unittest.TestCase):
 
     @classmethod
     def setUpClass(cls):
@@ -10,23 +10,30 @@ class BooksTests(unittest.TestCase):
         cls.url = cls.base_url + "/books"
 
     def setUp(self):
-        self.book_data = {"title": "Anna Karenina", "author": "Lev Tolstoy"}
-
-    def test_create_book(self):
-
-        response = requests.post(self.url, data=self.book_data)
-        self.assertEqual(response.status_code, 201)
-        response_body = response.json()
-        self.assertIn("id", response_body)
-        for key in self.book_data:
-            self.assertEqual(response_body[key], self.book_data[key])
-
-        self.book_data["id"] = response_body["id"]
-        self.assertEqual(self.book_data, response_body)
+        self.book_data = [
+            {"title": "Title1", "author": "Author1"},
+            {"title": "Anna Karenina", "author": "Lev Tolstoy"}
+        ]
+        self.book_ids = []
 
     def tearDown(self):
-        r = requests.delete(self.url+str(self.book_data["id"]))
-        print(r.status_code)
+        for book_id in self.book_ids:
+            requests.delete(self.url + "/" + str(book_id))
+            # print(r.status_code)
+        return
+
+    def test_create_book(self):  # CREATE
+        for item in self.book_data:
+            with self.subTest(item=item):
+                response = requests.post(self.url, data=item)  # create an item
+                self.assertEqual(response.status_code, 201)  # check code === 201
+                body = response.json()
+                self.assertIn("id", body)
+                self.book_ids.append(body["id"])  # save id for tearDown()
+                #
+                for key in item:
+                    self.assertEqual(str(item[key]).strip(), str(body[key]))
+        return True
 
 
 if __name__ == "__main__":
